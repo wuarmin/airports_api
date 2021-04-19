e3b55b65db112798fe511b7d9c7e2e5c9d6c52b6
wuarmin <armin@wurzweb.com>
2021-04-19 21:17:57 +0200

    CHG: add some fields to airport-factory

--- spec/factories/airport.rb
+++ spec/factories/airport.rb
@@ -8,5 +8,20 @@ FactoryBot.define do
     country_code { Faker::Address.country_code }
     country_name { Faker::Address.country }
     continent_name { Faker::Address.time_zone }
+    adm1_code { nil }
+    adm1_name { nil }
+    adm1_name_ascii { nil }
+    adm2_code { nil }
+    adm2_name { nil }
+    adm2_name_ascii { nil }
+    adm3_code { nil }
+    adm4_code { nil }
+    population { nil }
+    elevation { nil }
+    gtopo30 { nil }
+    timezone { nil }
+    gmt_offset { nil }
+    dst_offset { nil }
+    raw_offset { nil }
   end
 end
